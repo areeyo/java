ccc3e86acca9c08bf5536874bcd93a3806020083
areeyo <yakeemo2@gmail.com>
2016-04-10 01:04:41 +0700

    #02 create Spaceship

---
+++ Assignment/GameEngine.java
@@ -0,0 +1,77 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.Timer;
+
+
+
+public class GameEngine implements KeyListener{
+	GamePanel gp;
+		
+
+	private SpaceShip v;	
+	
+	private Timer timer;
+	
+	public GameEngine(GamePanel gp, SpaceShip v) {
+		this.gp = gp;
+		this.v = v;		
+
+		gp.sprites.add(v);
+
+		timer = new Timer(50, new ActionListener() {
+ 			@Override
+ 			public void actionPerformed(ActionEvent arg0) {
+ 				process();
+ 			}
+ 		});
+ 		timer.setRepeats(true);
+	}
+	
+	public void start(){
+		timer.start();
+	}
+
+	void controlVehicle(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_LEFT:
+			v.move(-1,0);
+			break;
+		case KeyEvent.VK_RIGHT:
+			v.move(1,0);
+			break;
+		case KeyEvent.VK_UP:
+			v.move(0,-1);
+			break;
+		case KeyEvent.VK_DOWN:
+			v.move(0,1);
+			break;
+		
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		controlVehicle(e);
+		
+	}
+
+	private void process(){
+		gp.updateGameUI();
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		//do nothing
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//do nothing		
+	}
+}
